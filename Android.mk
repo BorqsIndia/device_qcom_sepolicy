2d053571284209d02690185c221916f9e7b14ffa
Dhanalakshmi Siddani <dsiddani@codeaurora.org>
2015-03-26 11:06:03 +0530

    Seandroid: Add policy for dts configurator and notifier nodes
    Add policy for dts configurator service and notifier nodes.
    
    CRs-fixed: 780001
    
    Change-Id: I9a7909dcfb8afff91f7a52e39393f637845896c5

--- Android.mk
+++ Android.mk
@@ -94,7 +94,8 @@ BOARD_SEPOLICY_UNION := \
        dhcp.te \
        mediaserver_test.te \
        hbtp.te \
-       wfdservice.te
+       wfdservice.te \
+       dtsconfigurator.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084
