tusharind007/sepolicy_vndr

sepolicy: allow netdomain access to cnd and dpmd
Allow entities with netdomain permission
access to cnd and dpmd
Allow cnd to set socket options

CRs-Fixed: 802399

Change-Id: I0d3a6fed98e88d46bd6ceae0b3c654c1b53ac56f

Android.mk
@@ -96,7 +96,8 @@ BOARD_SEPOLICY_UNION := \
        energyawareness.te \
        usf.te \
        dtsconfigurator.te \
-       wfdservice.te
+       wfdservice.te \
+       net.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084

BorqsIndia/device_qcom_sepolicy

Android.mk
@@ -97,7 +97,8 @@ BOARD_SEPOLICY_UNION := \
        dtsconfigurator.te \
        platform_app.te \
        dtseagleservice.te \
-       nfc.te
+       nfc.te \
+       net.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084